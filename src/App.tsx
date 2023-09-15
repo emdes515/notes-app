5f52b6204cd72018e9b5cf741a830f7ed20e9060
Mateusz <mateuszhg5@gmail.com>
2023-09-15 23:37:47 +0200

    initial commit 🎆

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App(): React.ReactNode {
+	return (
+		<>
+			<div className="flex justify-center items-center h-screen">
+				<div className="mockup-code">
+					<pre data-prefix="$" className="text-success">
+						<code>There will be awesome notes app soon</code>
+					</pre>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default App
